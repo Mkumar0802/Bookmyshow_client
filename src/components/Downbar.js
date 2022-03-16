fb6b73a168654cf5b17643a8dfafe9d4c9c43baa
mkumar0802 <mkumar0802@gmail.com>
2022-03-16 15:52:05 +0530

    Footer

--- src/components/Downbar.js
+++ src/components/Downbar.js
@@ -2,7 +2,7 @@
 function Downbar() {
 
     return (
-        <div className="bg-slate-800  pl-20 text-white sm:hidden md:block ">
+        <div className=" w-full bg-slate-800  pl-20 text-white md:block justify-start hidden ">
             <div>
                 <ul className="flex space-x-3">
                     <li>
--- src/components/Partnerlink.js
+++ src/components/Partnerlink.js
@@ -11,11 +11,11 @@ function Supportlinks({ alignment = 'start' }) {
                 <div className="justify-center hidden"></div>
                 <div className='flex justify-center '>
 
-<div className='flex  space-x-44'>
-    <h1 className=''>
+<div className='flex  space-x-44 '>
+    <h1 className='justify-start hidden'>
         List Your Show,event,activity or a great experience?Partner with us 
-    </h1>
-    <button className=' rounded-md px-4 py-1  bg-red-600 hover:bg-red-700 '>Contact</button>
+    </h1 >
+    <button className=' justify-start hidden rounded-md px-4 py-1  bg-red-600 hover:bg-red-700 '>Contact</button>
     </div>
     </div>
     </div>
