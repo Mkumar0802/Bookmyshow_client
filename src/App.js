5c3baad07c54a19a62a552ec5fa3275542377482
mkumar0802 <mkumar0802@gmail.com>
2022-03-16 19:06:40 +0530

    Footer

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 
 import Navbar from "./components/Navbar"
 import Footer from "./components/Footer";
-
+import Slideshow from "./components/Imagesilder";
 
 function App() {
   return (
     <div className="App">
       <Navbar/>
+      <Slideshow/>
       <Footer/>
     </div>
   );
--- src/components/Downbar.js
+++ src/components/Downbar.js
@@ -3,13 +3,13 @@ function Downbar() {
 
     return (
         <div className=" w-full bg-slate-800  pl-20 text-white md:block justify-start hidden ">
-            <div>
-                <ul className="flex space-x-3">
+            <div className="flex">
+                <ul className="flex  space-x-3">
                     <li>
                         Movies
                     </li>
                     <li>
-                        Stream
+                        Stream<sup className="text-red-600 text-ellipsis text-xs">NEW</sup>
                     </li>
                     <li>
                         Events
@@ -27,9 +27,21 @@ function Downbar() {
                         Buzz
                     </li>
                 </ul>
-                <div>
-
-                </div>
+                    <ul className="flex space-x-3 ml-96">
+                        <li>
+                            List your Show<sup className="text-red-600 text-ellipsis text-xs">NEW</sup>
+                        </li>
+                        <li>
+                            Corporates
+                        </li>
+                        <li>
+                            Offers
+                        </li>
+                        <li>
+                            Gift Cards
+                        </li>
+                    </ul>
+                
             </div>
         </div>
     )
