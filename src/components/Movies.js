71d156104303d7aee0d586f417104001ec92c9ed
mkumar0802 <mkumar0802@gmail.com>
2022-03-18 10:46:55 +0530

    Button fix

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -22,50 +22,49 @@ const Main = () => {
         getData();
     }, []);
     return (
-        
-            <div className="w-auto h-auto bg-slate-700  ">
-                <div>
-                    <h2 className="px-16 pt-5  text-xl   md:text-2xl font-bold text-white  ">Movies</h2>
-                </div>
 
-
-                <div className=" pr-10  mx-10   grid-flow-row md:grid grid-cols-5 gap-10   ">
-                    {movieList.map((val) => {
-                        return (
-                            <div className="p-1 mr-2  m-5  bg-white  rounded-lg  max-w-sm  sm:rounded-lg  " key={val.index}>
-                                <img
-                                    className="md:h-60 object-cover rounded-xl md:w-full h-70  h-2/6 w-full    "
-                                    alt="Movies"
-                                    src={val.pic}
-                                ></img>
-                                <div className="p-2">
-                                    <p className="text-base md:text-lg font-bold  ">{val.name}</p>
-                                    <p className=" font-bold">
-                                        Certificate : {val.certificate}
-                                    </p>
-                                    <p className="text-lg">
-                                        {" "}
-                                        <span className="font-bold">  Language : </span> {val.language}
-                                    </p>
-                                    <div className="">
-                                    
-                                    </div>
-                                </div>
-
-                               <Button/>
-
-                            </div>
-                            
-
-                        );
-                    })}
-                </div>
-                
+        <div className="w-auto h-auto bg-slate-700  ">
+            <div>
+                <h2 className="px-16 pt-5  text-xl   md:text-2xl font-bold text-white  ">Movies</h2>
             </div>
-            
-                
-           
-        
+
+            <div className=" pr-10  mx-10     grid-flow-row md:grid-cols-3 gap-5  lg:grid grid-cols-5  ">
+                {movieList.map((val) => {
+                    return (
+                        <div className="p-1 mr-2  m-5  bg-white  rounded-lg  max-w-sm  sm:rounded-lg  " key={val.index}>
+                            <img
+                                className=" object-cover rounded-xl md:w-full  md:h-80  h-5/6 w-full   "
+                                alt="Movies"
+                                src={val.pic}
+                            ></img>
+                            <div className="p-2">
+                                <p className="text-base md:text-lg font-bold  ">{val.name}</p>
+                                <p className=" font-bold">
+                                    Certificate : {val.certificate}
+                                </p>
+                                <p className="text-lg">
+                                    {" "}
+                                    <span className="font-bold">  Language : </span> {val.language}
+                                </p>
+                                <div className="">
+
+                                </div>
+                            </div>
+
+                            <Button />
+
+                        </div>
+
+
+                    );
+                })}
+            </div>
+
+        </div>
+
+
+
+
     );
 };
 
