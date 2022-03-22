9c96b6197c2f5d085e9644844c602067a9a911be
mkumar0802 <mkumar0802@gmail.com>
2022-03-22 19:38:37 +0530

    payment

--- src/Pages/Payment.jsx
+++ src/Pages/Payment.jsx
@@ -13,17 +13,17 @@ function Payment() {
             </div>
             <div className="md:flex md:justify-center p-5  grid grid-rows-2 justify-center">
                 <form class="w-full max-w-lg">
-                    <div class="flex flex-wrap -mx-3 mb-6">
+                    <div class="flex flex-wrap mx-3 mb-6">
                         <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
                             <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-first-name">
                                 Name
                             </label>
-                            <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" id="grid-first-name" type="text" placeholder="Cardholder Name" />
+                            <input class="appearance-none block w-2/3 bg-gray-200 text-gray-700 border border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" id="grid-first-name" type="text" placeholder="Cardholder Name" />
                             <p class="text-red-500 text-xs italic">Please fill out this field.</p>
                         </div>
 
                     </div>
-                    <div class="flex flex-wrap -mx-3 mb-6">
+                    <div class="flex flex-wrap mx-3 mb-6">
                         <div class="w-1/3 px-3">
                             <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-password">
                                 Card Number
@@ -32,8 +32,8 @@ function Payment() {
                             <p class="text-gray-600 text-xs italic">Enter 16 digit Card number</p>
                         </div>
                     </div>
-                    <div class="flex flex-wrap -mx-3 mb-2">
-                        <div class="w-full md:w-1/3 px-3 mb-6 md:mb-0">
+                    <div class="flex flex-wrap mx-3 mb-2">
+                        <div class="w-1/4 md:w-1/3 px-3 mb-6 md:mb-0">
                             <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-city">
                                 Expire date
                             </label>
@@ -41,7 +41,7 @@ function Payment() {
                         </div>
 
 
-                        <div class="w-full md:w-1/3 px-3 mb-6 md:mb-0">
+                        <div class="w-1/4 md:w-1/3 px-3 mb-6 md:mb-0">
                             <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-zip">
                                 CVV
                             </label>
@@ -61,7 +61,7 @@ function Payment() {
                             <button className="p-5"> {count} </button>
                             <button onClick={() => setCount(count + 1)}   className={`  ${count>=100 ? 'bg-red-600 opacity-50 cursor-not-allowed px-4 py-1 rounded-md ' : 'rounded-md px-4 py-1 my  bg-red-600 hover:bg-red-700'} `}>+</button>
                         </div>
-                        <div className="justify-center py-5 px-2">
+                        <div className="justify-center py-5 px-4">
                             <button     className={`  ${count===0 ? 'bg-red-600 opacity-50 cursor-not-allowed px-4 py-1 rounded-md ' : 'rounded-md px-4 py-1 my  bg-red-600 hover:bg-red-700'} `}>PAY ₹{count*175} </button>
                         </div>
                     </div>
